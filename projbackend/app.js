1fc2f217c2144fe627a380aa617776eac8fa38e4
sujan <sujanpoojary884@gmail.com>
2020-04-05 21:33:59 +0530

    database connection and env variable setup.!

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,1 +1,25 @@
 
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+.then(() => {
+    console.log('DB_CONNECTED.!');
+})
+.catch(err => {
+    console.log(err);
+});
+
+
+const port = process.env.PORT || 6000;
+app.listen(port, () => {
+    console.log('server running on port 6000...');
+});
+
