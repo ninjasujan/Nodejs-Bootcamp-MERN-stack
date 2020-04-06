3b41dc4d4239efaba8cf7804a596ee89525d5902
sujan <sujanpoojary884@gmail.com>
2020-04-06 16:21:39 +0530

    validation of input done.!

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,7 +1,15 @@
 
 const User = require('../models/user');
+const {check, validationResult} = require('express-validator');
 
 exports.signup = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(422).json({
+            error: errors.array()[0].msg,
+            status: 'validationn failed'
+        });
+    }
     const user = new User(req.body);
     user.save()
         .then(user => {
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,15 @@
 
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
 
 const authController = require('../controllers/auth');
 
-router.post('/signup', authController.signup);
+router.post('/signup', [
+    check('name', 'invalid name field.!').isLength({min: 3}),
+    check('email', 'invalid email').isEmail(),
+    check('password', 'not a valid password').exists()
+        .isLength({min: 5})
+], authController.signup);
 
 module.exports = router;
