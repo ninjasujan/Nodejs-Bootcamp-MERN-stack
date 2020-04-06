40fa649af46a749b333e22d859467b5144dff74c
sujan <sujanpoojary884@gmail.com>
2020-04-06 12:37:11 +0530

    MVC architectural code setup.!

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,9 +1,19 @@
 
+// requiring core module and npm module
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// importing routes
+const authRoutes = require('./routes/auth');
+
+// middleware
+app.use(bodyParser.json());
+
+app.use('/api', authRoutes);
+
 
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -12,14 +22,17 @@ mongoose.connect(process.env.DATABASE, {
 })
 .then(() => {
     console.log('DB_CONNECTED.!');
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+        console.log(`server running on port ${port}...`);
+    });
 })
 .catch(err => {
     console.log(err);
 });
 
 
-const port = process.env.PORT || 6000;
-app.listen(port, () => {
-    console.log('server running on port 6000...');
-});
+
+
+
 
---
+++ projbackend/controllers/auth.js
@@ -0,0 +1,8 @@
+
+exports.signup = (req, res, next) => {
+    res.json({
+        user: "sujan",
+        email: "sujan@gmail.com",
+        status: "loggedIn"
+    })
+};
